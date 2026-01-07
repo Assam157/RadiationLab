11687a09d739ce032040657e87fc45d87b181660
Assam157 <asasm8106@gmail.com>
2026-01-07 15:01:49 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RadiationPhysicsLab from "./components/RaadiationPhysicsLab";
+
+function App() {
+  return (
+    <div style={{ minHeight: "100vh", background: "#000" }}>
+      <RadiationPhysicsLab />
+    </div>
+  );
+}
+
+export default App;
