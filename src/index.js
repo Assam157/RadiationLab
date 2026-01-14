71505bbcd175c1a853e6781f01be36d9552be8aa
Assam157 <asasm8106@gmail.com>
2026-01-14 19:51:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(() => console.log("Service Worker registered"))
+      .catch(err => console.error("SW registration failed", err));
+  });
+}
+
  
 
 
