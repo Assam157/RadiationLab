559577727cd9966f1e9ee1eedcbcd590a4fe117f
Assam157 <asasm8106@gmail.com>
2026-01-22 13:37:49 +0530

    Update DigitalGateLogic.jsx

--- src/components/DigitalGateLogic.jsx
+++ src/components/DigitalGateLogic.jsx
@@ -20,7 +20,7 @@ const gates = {
 /* ================= BULB SPRITE ================= */
 
 const bulbImg = new Image();
-bulbImg.src = "/bulby.png";
+bulbImg.src = "./prism.png";
 
 export default function DigitalGateLab() {
   const canvasRef = useRef(null);
@@ -271,6 +271,7 @@ export default function DigitalGateLab() {
     </div>
   );
 }
+
 
 
 
